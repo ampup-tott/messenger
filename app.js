6c04aab2ff362aaa2e2f6127b9e71d2fabfa6397
Abel Tran <ampup.tott@gmail.com>
2020-05-13 21:11:41 +0700

    add project

---
+++ app.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  let window = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false
+    }
+  });
+
+  window.setMenu(null);
+  window.loadURL('https://www.messenger.com');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+})
